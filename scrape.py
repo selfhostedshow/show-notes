57112fcee7f6fd6918eb5d4c8705076f027a2d96
IronicBadger <alexktz@gmail.com>
2021-07-18 14:26:15 -0400

    scraping basics ok
    Co-authored-by: Jake Howard <RealOrangeOne@users.noreply.github.com>

--- scrape.py
+++ scrape.py
@@ -1,29 +1,43 @@
 import requests
 from bs4 import BeautifulSoup
 from jinja2 import Template
+import shutil
+import os
 
 episode_numbers = list(range(0, 1))
-hosts = {"Title":[],"Link":[]}
+hosts = []
 tags = []
 desc_links = []
 
+BASE_URL = "https://selfhosted.show"
+
+api_data = requests.get(BASE_URL + "/json").json()
+
+for episode in api_data['items']:
+    episode_number = api_data['']
+
 for episode in episode_numbers:
+    realepnumber = episode+1
+
     # read in show notes for each episode
-    page = requests.get('https://selfhosted.show/' + format(episode+1))
+    page = requests.get(f"{BASE_URL}/{realepnumber}")
     soup = BeautifulSoup(page.content, 'html.parser')
 
     # scrape episode tags
     for link in soup.find_all("a", class_="tag"):
-        #print(link.get('href'))
-        tags.append(link.get('href'))
+        tags.append({
+            "link": BASE_URL + link.get('href'),
+            "text": link.get_text().strip()
+        })
         
 
     # scrape hosts
     for host in soup.find_all("ul", class_="episode-hosts"):
         for link in host.find_all("a"):
-            hosts["Title"].append(link.get('title'))
-            hosts["Link"].append(link.get('href'))
-            #hosts.append(link.get('href'), link.get('title'))
+            hosts.append({
+                "title": link.get('title'),
+                "link": BASE_URL + link.get('href')
+            })
 
     # scrape about this episode description
     for item in soup.find_all("div", class_="split-primary prose"):
@@ -37,12 +51,27 @@ for episode in episode_numbers:
                 desc_links.append(link)
 
     # grab episode stats
-    for stat in soup.find_all("div", class_="column"):
-        print(stat.p)
+    #for stat in soup.find_all("div", class_="column"):
+        #print(stat.p)
 
     # scrape episode metadata (air date, runtime, etc)
+
+
+    template = Template(open('templates/episode.md.j2').read())
+    output = template.render({"desc_links": desc_links, "tags": tags, "hosts": hosts})
+
+    # Remove any outputs from a previous run
+    try:
+        shutil.rmtree("output")
+        os.mkdir("output")
+    except:
+        pass
+
+    with open('output/episode%s.md' % realepnumber, 'w') as f:
+        f.write(output)
 
 #print(tags)
 #print(hosts)
 #print(description)
-print(desc_links)
+#print(desc_links)
+
